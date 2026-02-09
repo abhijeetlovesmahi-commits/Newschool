177a186bb439ff4d973c4b418d89222a1ccf3987
Abhijeet <abhijeetlovesmahi@gmail.com>
2026-02-09 12:31:52 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,49 +1,32 @@
 import { auth, db } from "./firebase.js";
-import { 
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { ref, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// SIGN UP
-window.signUpUser = function() {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let pass = document.getElementById("password").value;
-  let role = document.getElementById("role").value;
+window.signUpUser = function () {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const role = document.getElementById("role").value;
 
-  createUserWithEmailAndPassword(auth, email, pass)
+  if (!email || !password || !name) {
+    alert("All fields required");
+    return;
+  }
+
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      let user = userCredential.user;
+      const uid = userCredential.user.uid;
 
-      // SAVE USER INFO IN DATABASE
-      set(ref(db, "users/" + user.uid), {
-        name: name,
-        email: email,
-        role: role
+      set(ref(db, "users/" + uid), {
+        name,
+        email,
+        role
       });
 
-      alert("Signed up successfully!");
-      window.location = "login.html";
+      alert("Signup Successful");
+      window.location.href = "login.html";
     })
-    .catch((err) => {
-      alert(err.message);
-    });
-};
-
-// LOGIN
-window.loginUser = function() {
-  let email = document.getElementById("email").value;
-  let pass = document.getElementById("password").value;
-
-  signInWithEmailAndPassword(auth, email, pass)
-    .then((userCredential) => {
-      let user = userCredential.user;
-      alert("Login success!");
-      window.location = "dashboard.html"; // We’ll change this later
-    })
-    .catch((err) => {
-      alert(err.message);
+    .catch((error) => {
+      alert(error.message);
     });
 };
